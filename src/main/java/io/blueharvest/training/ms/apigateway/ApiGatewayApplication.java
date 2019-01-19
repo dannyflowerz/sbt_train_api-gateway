67e20f51b58389953c09fe50997a55ee865c39a9
dannyflowerz <daniel.gabor.flores@gmail.com>
2019-01-19 22:04:45 +0100

    implementation

---
+++ src/main/java/io/blueharvest/training/ms/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package io.blueharvest.training.ms.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient  // also works without this annotation just because the dependency and configuration are there
+@EnableZuulProxy // see configured routes at http://localhost:8080/actuator/routes
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
+
